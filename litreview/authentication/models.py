60ff8ba023405b7e6a29d93e31c41f2db8b4dadf
Frederic THOUVENOT <frederic.tnt78@gmail.com>
2022-03-23 18:40:38 +0100

    Initialisation projet django

---
+++ litreview/authentication/models.py
@@ -0,0 +1,8 @@
+# authentication/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """ Utilisateur générique django + photo de profil """
+    profile_photo = models.ImageField(verbose_name='Photo de profil')
